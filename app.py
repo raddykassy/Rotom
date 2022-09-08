e7348d58b2e567d93b15e93d13ec3e809b515294
raddykassy <proraddy1015@gmail.com>
2022-09-08 10:24:42 +0900

    app.py path設定

--- app.py
+++ app.py
@@ -5,3 +5,27 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def index():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+@app.route('/inquiry')
+def inquiry():
+    return render_template('inquiry.html')
+
+@app.route('/plan')
+def plan():
+    return render_template('plan.html')
+
+@app.route('/serach')
+def search():
+    return render_template('search.html')
