b10c8133767a57c78e6264e387366e2069a7b789
raddykassy <proraddy1015@gmail.com>
2022-09-16 19:58:19 +0900

    post.html　情報キャッチ機能途中段階

--- static/javascripts/search.js
+++ static/javascripts/search.js
@@ -1,6 +1,7 @@
 // callback function
 var input_num;
 let autocomplete = {};
+let autocp = [];
 
 function initMap() {
     const test_place = { lat: 34.6460706, lng: 135.5134771 };
@@ -29,51 +30,40 @@ function initMap() {
     // https://developers.google.com/maps/documentation/javascript/reference/places-widget#SearchBox
     // 1番目のinputタグにautocomplete機能を搭載（1番目の要素のみid = "autocomplete"をもつため）
     first_input = document.getElementById("autocomplete");
-    autocomplete[0] = new google.maps.places.Autocomplete(first_input, option);
+    // autocomplete[0] = new google.maps.places.Autocomplete(first_input, option);
+    autocomplete = new google.maps.places.Autocomplete(first_input, option);
 
+    // let autocp = [];
 
     //ボタンが押された回数だけautocompleteクラスを持ったinputタグができる(addplan.js)→2番目の要素を持ったものを取得
     var addPlanBtn = document.getElementById('add-plan-btn');
 
     // 後でdocument全体からクラス検索ではなく、あるidをもつ親要素を指定して検索範囲を限定する（リファクタリングで実装）https://www.sejuku.net/blog/68588
     addPlanBtn.addEventListener('click', function() {
-        countPlaces = document.getElementsByClassName("autocomplete"); //autocompleteクラスを持つ要素を取得
-        input_num = countPlaces.length; //autocompleteクラスを持つ要素の数を取得
-        num = input_num - 1; //autocompleteのインスタンスをinputの順番に応じたキーに対して配列に格納するので、配列のゼロインデックスに合わせるため1を引く
-        // console.log(countPlaces[num]);
-        autocomplete[num] = new google.maps.places.Autocomplete(countPlaces[num], option); //autocomplete機能をinputタグに付加
+        ac_class = document.getElementsByClassName("autocomplete"); //autocompleteクラスを持つ要素を取得
+        num_ac_class = ac_class.length; //autocompleteクラスを持つ要素の数を取得
+        // num = input_num - 1; //autocompleteのインスタンスをinputの順番に応じたキーに対して配列に格納するので、配列のゼロインデックスに合わせるため1を引く
+        specific_ac = "autocomplete_" + num_ac_class;
 
-        // autocompleteはオブジェクトなので、forEachを使うためにこのような記述が必要?
-        // Object.keys(autocomplete).forEach(key => {
-        //     // console.log(key);
-        //     console.log(autocomplete[key]);
-        //     autocomplete[key].addListener("place_changed", onPlaceChanged(autocomplete[key]));
-        // });
+        ac_input = document.getElementById(specific_ac); //autocompleteクラスを持つ要素を取得
+        // console.log(auto_input);
+        // console.log(countPlaces[num]);
+        autocp[num_ac_class] = new google.maps.places.Autocomplete(ac_input, option);//autocomplete機能をinputタグに付加
+        ac_output = autocp[num_ac_class]; //配列に入れたものを別の変数に移したものをaddListenerすればできる。演算子の優先順位？？
+
+        ac_output.addListener("place_changed", onPlaceChanged);
+
 
         // console.log(autocomplete[num]);
-
-        autocomplete[num].addListener("place_changed", onPlaceChanged(autocomplete[num]));
     });
 
 
+    // 検索候補がクリックされた際のイベントの定義
+    function onPlaceChanged() {
 
+        var place = ac_output.getPlace();
 
-    // autocomplete.forEach(function(elem) {
-    //     console.log(elem)
-    // }
-        // elm.addListener("place_changed", console.log([].slice.call(autocomplete).indexOf(elm)));
-
-    // https://developers.google.com/maps/documentation/javascript/reference/places-widget#Autocomplete.place_changed
-    // autocomplete.addListener("place_changed", onPlaceChanged); //event
-
-    // // 検索候補がクリックされた際のイベントの定義
-    function onPlaceChanged(content) {
-        // 選択された場所の情報を取得
-        // console.log(content.getPlace());
-        var place = content.getPlace();
-
-    //     // place.キー名で情報受け取れる
-        // console.log(place);
+        console.log(place);
     //     // console.log(place.website);
     //     // console.log(document.getElementById("site_url").getAttribute("src"))
 
