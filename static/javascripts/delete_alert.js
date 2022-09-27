34a3f907442e60dfc8650b10fec2af77ab1b516a
hiroto0000 <h.hran4076@gmail.com>
2022-09-27 21:50:34 +0900

    9/27 delete_alert.jsわからん

---
+++ static/javascripts/delete_alert.js
@@ -0,0 +1,20 @@
+//function deleteComfirm(){
+    var deleteBtn = document.getElementById('delete-btn');
+    deleteBtn.addEventListener('click', function(){
+        alert("aaa");
+//         result = window.confirm("この投稿を消去しますか？");
+    
+//         if(result){
+//             console.log("ok");
+//             //return true;
+//         }
+//         else{
+//             colsole.log("no");
+//             //return false;
+//         }
+
+
+// 
+}
+)
+//}
