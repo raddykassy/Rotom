321ee87e001776c7a46fe6c8265a063a961d1602
Yuguchi Kousuke <86179144+Kou0805@users.noreply.github.com>
2022-09-29 22:46:26 +0900

    いったん保存

--- static/javascripts/jquery.validate.js
+++ static/javascripts/jquery.validate.js
@@ -3,12 +3,87 @@ $.validator.setDefaults({
 });
 
 $().ready(function() {
+  $('#login_form').validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        password: true,
+      }
+    },
+      messages: {
+        email: {
+          required: 'メールアドレスが入力されていません',
+          email: '正しいemail形式で入力してください'
+        },
+        password: {
+          required: '入力されていません',
+          password:'パスワードが入力されていません'
+        }
+      }
+  });
+});
+
+$().ready(function() {
+  $("#register_id").validate({
+    rules: {
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        password: true,
+      },
+      confirm_password: {
+        required: true,
+        password: true,
+        equalTo: '[name=password]',
+      },
+      username: {
+        required: true,
+      },
+    },
+      messages: {
+        email: {
+          required: 'メールアドレスが入力されていません',
+          email: '正しいemail形式で入力してください'
+        },
+        password: {
+          required: '入力されていません',
+          password:'パスワードが入力されていません'
+        },
+        confirm_password: {
+          required:'入力されていません',
+          password: 'パスワードが入力されていません',
+          equalTo: '同じパスワードを入力してください'
+        },
+        username: {
+          required: 'ユーザーネームが入力されていません',
+        },
+      },
+          // エラーメッセージ出力箇所
+      errorPlacement: function(error, element){
+        var name = element.attr('name');
+        error.appendTo($('.is-error-'+name));
+    },
+
+      errorElement: "span",
+      errorClass: "is-error",
+  });
+});
+
+
+$().ready(function() {
   $("#form_id").validate({
   rules: {
-      plantitle: {
+      plan_title: {
         required: true
       },
-      vlogurl: {
+      vlog_url: {
         required: true,
         url: true,
       },
@@ -29,10 +104,10 @@ $().ready(function() {
       },
   },
   messages: {
-      plantitle: {
+      plan_title: {
         required: "プランタイトルを入力してください"
       },
-      vlogurl: {
+      vlog_url: {
         required:"入力してください",
         url:"URL形式で入力してください"
       },
@@ -51,8 +126,15 @@ $().ready(function() {
       description: {
         required: '場所を入力してください'
       },
-    }
-  });
-  // propose username by combining first- and lastname
+    },
 
+      // エラーメッセージ出力箇所
+      errorPlacement: function(error, element){
+        var name = element.attr('name');
+        error.appendTo($('.is-error-'+name));
+    },
+
+      errorElement: "span",
+      errorClass: "is-error",
+  });
   });
