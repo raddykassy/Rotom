c27a3e4bfe403d4c170e56b82d405b6d4df72e2a
hiroto0000 <h.hran4076@gmail.com>
2022-12-18 16:39:33 +0900

    12/18 修正

--- app.py
+++ app.py
@@ -112,24 +112,24 @@ def get_dict_resultset(sql, *args):
     return dict_result
 
 # 開発用
-def psycopg2_connect():
-    conn = psycopg2.connect('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format( 
-                user="postgres",        #ユーザ
-                password=pas,  #パスワード
-                host="localhost",       #ホスト名
-                port="5432",            #ポート
-                dbname="postgres"))    #データベース名
-    return conn
+# def psycopg2_connect():
+#     conn = psycopg2.connect('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format( 
+#                 user="postgres",        #ユーザ
+#                 password=pas,  #パスワード
+#                 host="localhost",       #ホスト名
+#                 port="5432",            #ポート
+#                 dbname="postgres"))    #データベース名
+#     return conn
 
 # デプロイ用
-#def psycopg2_connect():
-#    conn = psycopg2.connect('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format( 
-#                user="rotom_qtnb_user",        #ユーザ
-#                 password=pas,  #パスワード
-#                 host="dpg-cefa7r1gp3jk7mh3d7qg-a",       #ホスト名
-#                 port="5432",            #ポート
-#                 dbname="rotom_qtnb"))    #データベース名
-#     return conn
+def psycopg2_connect():
+    conn = psycopg2.connect('postgresql://{user}:{password}@{host}:{port}/{dbname}'.format( 
+               user="rotom_qtnb_user",        #ユーザ
+                password=pas,  #パスワード
+                host="dpg-cefa7r1gp3jk7mh3d7qg-a",       #ホスト名
+                port="5432",            #ポート
+                dbname="rotom_qtnb"))    #データベース名
+    return conn
 
 @app.route('/')
 def index():
